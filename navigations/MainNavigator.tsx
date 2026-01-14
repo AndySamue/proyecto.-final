115d238c7ad87b8ff8acd16c93e9cc52c676a450
Andy <andysamuel2002lg@gmail.com>
2026-01-14 15:08:39 -0500

    Agregar iconos en navegación

--- navigations/MainNavigator.tsx
+++ navigations/MainNavigator.tsx
@@ -4,15 +4,56 @@ import { NavigationContainer } from "@react-navigation/native";
 import RegisterSceen from "../screens/RegisterSceen";
 import LoginScreen from "../screens/LoginScreen";
 import GameScreen from "../screens/GameScreen";
+import { Entypo, FontAwesome } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
 function MyTabs() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Register" component={RegisterSceen} />
-      <Tab.Screen name="Login" component={LoginScreen} />
-      <Tab.Screen name="Game" component={GameScreen} />
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: "#233D4D",
+          height: 60,
+          borderTopLeftRadius: 20,
+          borderTopRightRadius: 20,
+          position: "absolute",
+        },
+        tabBarActiveTintColor: "#F5FBE6",
+        tabBarInactiveTintColor: "#215E61",
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: "bold",
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Iniciar Sesión"
+        component={LoginScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name="user" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Registrarse"
+        component={RegisterSceen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Entypo name="login" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Juego"
+        component={GameScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Entypo name="game-controller" size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
